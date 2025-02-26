1b52831c6e61ebcbf148b2cf598d8f940a119690
saad-dot-com-np <fds.adhikari@gmail.com>
2025-02-26 10:53:32 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Basic form submission handling
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert('Thank you for your message! We will get back to you soon.');
+  this.reset();
+});
